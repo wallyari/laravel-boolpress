d7604f301c6e3ca62892f51075cb3a0108d80d7b
ariannadepasquale <depasqualearianna@gmail.com>
2022-10-07 23:32:35 +0200

    Add: componenti - rotte middleware

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,7 +3,8 @@
 @section('content')
 
 <div class='container'>
-    <h1>Benvenuto nell'area amministrativa {{Auth::user()->name}}</h1>
+    <h1>Benvenuto nell'area amministrativa {{Auth::user()
+    ->name}}</h1>
 </div>
     
 @endsection
