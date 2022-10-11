1524619ed17103253ed62f097600041f27affb9b
ariannadepasquale <depasqualearianna@gmail.com>
2022-10-12 00:49:14 +0200

    Add category in Show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<div class="container m-5">
+<div class="container text-center">
 <div class="card">
     <div class="card-header">
         <h3>Title: {{$post->title}}</h3>
@@ -8,6 +8,7 @@
     <div class="card-body">
         <blockquote class="blockquote mb-0">
         <p> Descripion: {{$post->content}}</p>
+        <p> Category: {{$post->category?-$post->$category->name:'Unavailable'}}</p>
         <footer class="blockquote-footer ">Slug: <cite title="Source Title"> {{$post->slug}}</cite></footer>
         </blockquote>
     </div>
@@ -15,10 +16,5 @@
     <div class= "card-footer m-3 d-flex justify-content-center">
         <a href="{{route('admin.posts.index')}}" class="btn btn-danger">Back</a>
     </div>
-
-
-</div>
-
-
-    
+</div>   
 @endsection
