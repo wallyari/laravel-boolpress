0464b074245ae2020341e59334e726d0eb3b6590
ariannadepasquale <depasqualearianna@gmail.com>
2022-10-18 02:25:47 +0200

    Add Navbar&Partial Card in Main

---
+++ resources/js/components/MyMain.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="card col-12 mb-3" v-for="(post,index) in posts" :key="index">
+                <img src="#" class="card-img-top" alt="#">
+                <div class="card-body">
+                    <h5 class="card-title">{{post.title}}</h5>
+                    <p class="card-text">{{post.content}}</p>
+                    <a href="#" class="btn btn-primary">Read more</a>
+                </div>
+            </div>
+        </div> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyMain',
+    data(){
+        return {
+        posts: []
+         }
+    },
+    methods:{
+        getPosts(){
+            axios.get('/api/posts')
+            .then((response) => {
+                this.posts =response.data.results;
+                console.log(response.data.results);
+            });
+        }
+    },
+    mounted(){
+        this.getPosts();
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
         <MyHeader/>
+        <MyMain/>
+        
     </div>
 </template>
 
 <script>
     import MyHeader from '../components/MyHeader.vue'
+    import MyMain from '../components/MyMain.vue';
     export default {
         name: 'App',
         components: { 
             MyHeader,
+            MyMain
         }
     }
 </script>
